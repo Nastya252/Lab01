b7d2b3abac09e473380169e5133eceb96210dfee
Настя <nastygit@mail.ru>
2026-09-19 23:42:20 +0300

    Lab01 final: student visitka program

--- Visitka/Program.cs
+++ Visitka/Program.cs
@@ -1,26 +1,17 @@
 ﻿//Визитка студента
 Console.WriteLine("  ВИЗИТНАЯ КАРТОЧКА СТУДЕНТА");
-Console.Write("Введите ваше ФИО: ");
-string name = Console.ReadLine();
-Console.Write("Введите вашу группу: ");
-string group = Console.ReadLine();
-Console.Write("Введите ваш курс: ");
-string course = Console.ReadLine();
-int coursse = int.Parse(course);
-Console.Write("Введите вашу специальность: ");
-string speciality = Console.ReadLine();
-Console.Write("Введите ваш средний балл: ");
-string middlemark = Console.ReadLine();
-bool bigger = double.TryParse(middlemark, out double middle);
-Console.Write("Сколько недель прошло с начала учебы: ");
-string weekafter = Console.ReadLine();
-int weeksafter = int.Parse(weekafter);//преобразуем число недель в числовой тип данных, чтобы в дальнейшем произвести вычисления
+string name = "Пономарева Анастасия Андреевна";
+string group = "ИСП252";
+int course = 2;
+string speciality = "09.02.07";
+double middlemark = 4.5;
+int weekafter = 3; // недель после каникул
 const int weeks = 17; // принимаем количество недель за константу, потому что оно не меняется
-
+bool bigger = middlemark >= 4;
 Console.WriteLine($"ФИО: {name}");
 Console.WriteLine($"Группа: {group}");
-Console.WriteLine($"Курс: {coursse}");
+Console.WriteLine($"Курс: {course}");
 Console.WriteLine($"Специальность: {speciality}");
 Console.WriteLine("\n");
 Console.WriteLine($"Средний балл >= 4: {bigger}");
-Console.WriteLine($"Учебных недель осталось в семестре: {weeks - weeksafter}"); // вычисляем количество оставшихся недель
+Console.WriteLine($"Учебных недель осталось в семестре: {weeks - weekafter}"); // вычисляем количество оставшихся недель
